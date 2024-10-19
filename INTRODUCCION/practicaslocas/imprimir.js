b95cf5c581ee4ecc266c9773a83353d4a967951a
Osmely sangronis <osmely2610@gmail.com>
2024-10-19 11:18:22 -0500

    cambiamos el links del archivo js para comprobar practica

---
+++ INTRODUCCION/practicaslocas/imprimir.js
@@ -0,0 +1,14 @@
+for (let i=1;i<=10;i++){
+    console.log(i);
+}
+
+for(let i=1;i<=20;i++){
+    if(i%2===0){
+        console.log(i);
+    }
+}
+
+let numero=5;
+for (let i=1;i<=10;i++){
+    console.log(numero*i);
+};
