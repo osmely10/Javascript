3ea692490d3fe9554ec78859361ab0f963894ce7
Osmely sangronis <osmely2610@gmail.com>
2024-10-31 13:04:01 -0500

    practica en el archivo saludo

--- INTRODUCCION/practicaslocas/saludo.js
+++ INTRODUCCION/practicaslocas/saludo.js
@@ -1,2 +1,11 @@
 const nombre = prompt ('Escribe tu nombre');
 console.log(`Hola ${nombre}`);
+
+
+const numero1 = parseInt(prompt('Escribe un numero entero'));
+const numero2 = parseInt(prompt('Escribe un numero entero'));
+const numero3 = parseInt(prompt('Escribe un numero entero'));
+
+const entero = ((numero1 + numero2 + numero3)/3);
+
+console.log(entero);
