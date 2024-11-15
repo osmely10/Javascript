ce963120c2d4d60b078dc418867e082cba8c1338
Osmely sangronis <osmely2610@gmail.com>
2024-11-15 14:30:24 -0500

    Practica de javascript y agregamos en el ejercicio del archivo repetido una funcion

--- INTRODUCCION/practicaslocas/repetido.js
+++ INTRODUCCION/practicaslocas/repetido.js
@@ -1,7 +1,11 @@
 function eliminar (){
     const numeros=[2,3,4,4,5,6,7,8,34,67,8,3];
+    const nuevonumeros=numeros.filter((numero,index)=>{
+        return numeros.indexOf(numero)===index;
+    });
+
+    console.log(nuevonumeros);
     
-console.log(numeros);
 };
 
 eliminar();
