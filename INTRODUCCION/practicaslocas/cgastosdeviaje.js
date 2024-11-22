096e1eb16f35ca652d9ea137d4777405599ee601
Osmely sangronis <osmely2610@gmail.com>
2024-11-22 14:49:16 -0500

    ejercicios practicos de javascript

---
+++ INTRODUCCION/practicaslocas/cgastosdeviaje.js
@@ -0,0 +1,8 @@
+const alojamiento=parseInt(prompt('Ingrese lo gastado en alojamiento'));
+const alimentacion=parseInt(prompt('Ingrese lo gastado en alojamiento'));
+const entretenimiento=parseInt(prompt('Ingrese lo gastado en alojamiento'));
+
+const gastostotal=alojamiento+alimentacion+entretenimiento;
+
+console.log(`Costo total del viaje fue: ${gastostotal}`);
+
---
+++ INTRODUCCION/practicaslocas/edadcanina.js
@@ -0,0 +1,4 @@
+const edadcanina=parseInt(prompt('Ingrese los años del canino'));
+const transformacion=edadcanina*7;
+
+console.log(`La edad del perro en años humanos es ${transformacion}`);
