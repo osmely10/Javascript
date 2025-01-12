1801c0f651d20e88bae606a44128559899a0a08d
Osmely sangronis <osmely2610@gmail.com>
2025-01-12 11:28:27 -0500

    Devuelta terminando el ejercicio orden descendente sumapar

---
+++ INTRODUCCION/practicaslocas/sumapar.js
@@ -0,0 +1,14 @@
+
+let suma=0
+for(let i=1;i<=50;i++){
+    if(i%2==0){
+        suma+=i
+    }
+}
+console.log(`la suma de los numeros pares es: ${suma}`)
+
+// Orden descendente
+
+for(let i=30; i>19; i--){
+    console.log(i)
+}
---
+++ INTRODUCCION/practicaslocas/triangulo.js
@@ -0,0 +1,8 @@
+let altura=9
+let fila=''
+for(let i=1;i<=altura;i++){
+    fila+='*'
+    console.log(fila)
+}
+
+
